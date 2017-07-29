36422d9108b950152f22434642fc290123403db4
Sam <snicholson@workanyware.co.uk>
2017-07-29 19:21:05 +0100

    Got multiplayer working

--- js/robot_handler.js
+++ js/robot_handler.js
@@ -1,17 +1,37 @@
 /**
  * The Robot handler
  */
-var Robot = function (startPosition, sprite, game) {
+var Robot = function (startPosition, sprite, game, playerNumber) {
     //Create the robot sprite
     this.robot = game.create.sprite(sprite, 'fly', {regX:'center', regY:'center', x:startPosition[0], y:startPosition[1]});
     game.stage.addChild(this.robot);
 
+    this.playerNumber = playerNumber;
 
     //Create the power bar
-    this.bar1 = new tine.ProgressBar('green', 'white', tine.BOTTOM_TO_TOP,415, 15);
+    switch (playerNumber) {
+        case 1:
+            var powerBarX = 32;
+            var powerBarY = game.canvas.height - 32;
+            this.leftKey = tine.keys.A;
+            this.rightKey = tine.keys.D;
+            this.sprintKey = tine.keys.SHIFT;
+            this.jumpKey = tine.keys.W;
+            break;
+        case 2:
+            var powerBarX = game.canvas.width - 48;
+            var powerBarY = game.canvas.height - 32;
+            this.leftKey = tine.keys.LEFT;
+            this.rightKey = tine.keys.RIGHT;
+            this.sprintKey = tine.keys.CTRL;
+            this.jumpKey = tine.keys.UP;
+            break;
+    }
+
+    this.bar1 = new tine.ProgressBar('green', 'white', tine.BOTTOM_TO_TOP, 415, 15);
     this.bar1.value = 100;
-    this.bar1.x = 32;
-    this.bar1.y = game.canvas.height - 32;
+    this.bar1.x = powerBarX;
+    this.bar1.y = powerBarY;
     game.stage.addChild(this.bar1);
 
     this.speedMultiplier = 1;
@@ -21,7 +41,7 @@ var Robot = function (startPosition, sprite, game) {
 
 Robot.prototype.update = function(game) {
 
-    if (game.keyboard.isDown(tine.keys.SHIFT)) {
+    if (game.keyboard.isDown(this.sprintKey)) {
         this.speedMultiplier       = ROBOT_SPRINT_MOVEMENT_MULTIPLIER;
         this.degredationMultiplier = ROBOT_SPRINT_MOVEMENT_MULTIPLIER;
     } else {
@@ -29,12 +49,17 @@ Robot.prototype.update = function(game) {
         this.degredationMultiplier = 1;
     }
 
-    if ((game.keyboard.isDown(tine.keys.A)) && (this.robot.x - (ROBOT_SIZE / 2) >= ROBOT_BOTTOM_OFFSET)) {
-        this.robot.x -= this.calculateMovementSpeed() * (60 * game.time.fdelta);
-        this.robot.gotoAndPlay('left');
-    } else if ((game.keyboard.isDown(tine.keys.D)) && (this.robot.x - (ROBOT_SIZE / 2) <= (game.canvas.width - ROBOT_BOTTOM_OFFSET))) {
-        this.robot.x += this.calculateMovementSpeed() * (60 * game.time.fdelta);
-        this.robot.gotoAndPlay('right');
+    if (game.keyboard.isDown(this.jumpKey) && this.robot.y > 0 + ROBOT_SIZE) {
+        this.robot.y -= this.calculateMovementSpeed() * (60 * game.time.fdelta);
+        this.changePower(ROBOT_JUMP_PENALTY);
+    } else if (this.robot.y < 576) {
+        this.robot.y += this.calculateMovementSpeed() * (60 * game.time.fdelta);
+    }
+
+    if ((game.keyboard.isDown(this.leftKey)) && (this.robot.x - (ROBOT_SIZE / 2) >= ROBOT_BOTTOM_OFFSET)) {
+        this.moveLeft(game)
+    } else if ((game.keyboard.isDown(this.rightKey)) && (this.robot.x - (ROBOT_SIZE / 2) <= (game.canvas.width - ROBOT_BOTTOM_OFFSET))) {
+        this.moveRight(game);
     } else {
         this.robot.gotoAndPlay('fly');
     }
@@ -44,6 +69,16 @@ Robot.prototype.update = function(game) {
     if (this.bar1.value <= 0) {
         game.end();
     }
+};
+
+
+Robot.prototype.moveLeft = function(game) {
+    this.robot.x -= this.calculateMovementSpeed() * (60 * game.time.fdelta);
+    this.robot.gotoAndPlay('left');
+};
+Robot.prototype.moveRight = function(game) {
+    this.robot.x += this.calculateMovementSpeed() * (60 * game.time.fdelta);
+    this.robot.gotoAndPlay('right');
 };
 
 Robot.prototype.calculateMovementSpeed = function() {
